38cdb070d927385d3109eca7ef1a87deebd030ad
Luan MM Ribeiro <luan.ribeiro@locaweb.com.br>
2020-01-06 10:12:17 -0300

    Third class

---
+++ rental-cars-qsd2019/spec/features/visitor_view_manufacturers_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'Visitor view manufacturers' do
+  scenario 'successfully' do
+    # Testes de três estados - AAA
+
+    # Arrange - Criar dados
+    Manufacturer.create(name: 'Fiat')
+    Manufacturer.create(name: 'Volkswagen')
+
+    # Act - Executar ações
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+
+    # Assert - Verificar coisas
+    expect(page).to have_content('Fiat')
+    expect(page).to have_link('Voltar')
+  end
+
+  scenario 'and return to home page' do
+    Manufacturer.create(name: 'Fiat')
+    Manufacturer.create(name: 'Volkswagen')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+  end
+end
